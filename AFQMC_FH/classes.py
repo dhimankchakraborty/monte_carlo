2e640b55c20e61384f40cfe74816df2237582ef7
Dhiman Kumar Chakraborty <dhimankumarchakraborty@gmail.com>
2026-04-15 10:29:02 +0530

    Implemented non-interacting propagator for Hubbard model in AFQMC_FH in python

--- AFQMC_FH/classes.py
+++ AFQMC_FH/classes.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import expm
 
 
 
@@ -11,5 +12,37 @@ class slaterWaveFunction2Spin:
 
         self.wave_function_up = np.zeros((self.n_basis, self.n_particle_up))
         self.wave_function_down = np.zeros((self.n_basis, self.n_particle_down))
+
+    def wave_function_initialization(self, up_sates, down_states):
+        self.up_initializer = up_sates[:self.n_particle_up]
+        self.down_initializer = down_states[:self.n_particle_down]
+
+        for i in range(self.n_particle_up):
+            self.wave_function_up[:, i] = self.up_initializer[i]
+        
+        for i in range(self.n_particle_down):
+            self.wave_function_down[:, i] = self.down_initializer[i]
+
+
+
+class imaginaryTimePropagator:
+    def __init__(self, one_body_operator_matrix, delta_tau) -> None:
+        self.one_body_operator_matrix = one_body_operator_matrix
+        self.delta_tau = delta_tau
+        self.propagator = expm(-self.delta_tau * self.one_body_operator_matrix)
+        # self.propagator = np.exp(-self.delta_tau * self.one_body_operator_matrix) # Used exmp in code, if any problem arise, use np.exp to see if the result improves
+    
+    def apply_propagator(self, wave_function):
+        self.n_basis = wave_function.n_basis
+        self.n_particle_up = wave_function.n_particle_up
+        self.n_particle_down = wave_function.n_particle_down
+
+        modified_wave_function = slaterWaveFunction2Spin(self.n_basis, self.n_particle_up, self.n_particle_down)
+
+        modified_wave_function.wave_function_up = self.propagator @ wave_function.wave_function_up
+
+        modified_wave_function.wave_function_down = self.propagator @ wave_function.wave_function_down
+
+        return modified_wave_function
 
 
--- AFQMC_FH/functions.py
+++ AFQMC_FH/functions.py
@@ -44,3 +44,41 @@ def non_interacting_fermi_hubbard_ed(length_x, length_y, t, use_pbc=False):
 
     return H, eigenvalues, eigenvectors.transpose()
 
+
+def hubbard_kinetic_2d_matrix(length_x, length_y, t, use_pbc=False):
+    dim = length_x * length_y
+    K = np.zeros((dim, dim))
+
+    for x in range(length_x):
+        for y in range(length_y):
+
+            i = site_index(x, y, length_x)
+
+            x_plus = x + 1
+            if x_plus < length_x:
+                j = site_index(x_plus, y, length_x)
+                K[i, j] = -t
+                K[j, i] = -t
+            elif use_pbc:
+                j = site_index(0, y, length_x)
+                K[i, j] = -t
+                K[j, i] = -t
+
+            y_plus = y + 1
+            if y_plus < length_y:
+                j = site_index(x, y_plus, length_x)
+                K[i, j] = -t
+                K[j, i] = -t
+            elif use_pbc:
+                # Wrap around
+                j = site_index(x, 0, length_x)
+                K[i, j] = -t
+                K[j, i] = -t
+
+    for i in range(dim):
+        K[i, i] = 0
+    
+    return K
+
+
+
